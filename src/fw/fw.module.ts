0e234713900c4f738f83ac8dd794f6588afc8c40
soorajmehta <sooraj.maheta@gmail.com>
2017-04-12 08:21:21 -0400

    initial commit.

---
+++ src/fw/fw.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FrameworkBodyComponent } from "./framework-body/framework-body.component";
+import { ContentComponent } from './content/content.component';
+import { TitleBarComponent } from './title-bar/title-bar.component';
+import { FrameworkConfigService } from './services/framework-config.service';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { StatusBarComponent } from './status-bar/status-bar.component';
+import { ScreenService } from './services/screen.service';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    FrameworkBodyComponent,
+    ContentComponent,
+    TitleBarComponent,
+    TopBarComponent,
+    StatusBarComponent
+  ],
+  providers:[
+    FrameworkConfigService,
+    ScreenService
+  ],
+  exports:[
+    FrameworkBodyComponent
+  ]
+})
+export class FwModule { }
